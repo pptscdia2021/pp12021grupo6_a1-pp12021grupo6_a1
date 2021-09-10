51ffc61dec59f307f1eb14a977c86936c9ef6cc7
Nahulobo10 <gabrielnahuellobo@gmail.com>
2021-09-10 13:01:27 -0300

    commit30

--- Principal.py
+++ Principal.py
@@ -3,8 +3,11 @@ def suma(a, b, c):
     suma= a + b + c
     return suma
 
+def parametro (texto, valor):
+    print (str ("La impresion de texto y valor es: "), str (texto), int (valor))
 
 
 if __name__ == "__main__":
     print('Primer programa en Python')
     print("La suma es: ", str(suma(2,3,4)))
+    parametro("Julio",16)
