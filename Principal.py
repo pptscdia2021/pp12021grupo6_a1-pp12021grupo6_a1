1585c569dd8d9c3f988cd405a7d26fb196a49768
Bluelieta <julietabattauz42@gmail.com>
2021-09-14 00:22:55 -0300

    commit 11

--- Principal.py
+++ Principal.py
@@ -3,6 +3,10 @@ def suma(a, b, c):
     suma= a + b + c
     return suma
 
+def Resta (n1, n2):
+    resta= n1 - n2
+    return resta
+
 def parametro (texto, valor):
     print (str ("La impresion de texto y valor es: "), str (texto), int (valor))
 
@@ -16,3 +20,5 @@ if __name__ == "__main__":
     print("La suma es: ", str(suma(2,3,4)))
     parametro("Julio",16)
     print("El producto es: ", str(producto(2,3,5,2)))
+    print("Resultado de la resta: ", str(Resta(10,3)))
+    
