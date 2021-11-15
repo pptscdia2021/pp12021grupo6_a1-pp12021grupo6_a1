037da1189b65e82cb04ecd247fa55c97b3e26735
vanigc <67790456+vanigc@users.noreply.github.com>
2021-11-15 10:40:52 -0300

    commit order docs

--- TP2-WSyAPI/TP2-Parte_I/Comparacion.py
+++ TP2-WSyAPI/TP2-Parte_I/Comparacion.py
@@ -1,3 +1,4 @@
+# Se importan las librerías necesarias para la Comparación
 import matplotlib.pyplot as plt
 import pandas as pd
 from Graficar import grafcompar
@@ -6,6 +7,7 @@ from Graficar import grafcompar
 bMadrid = pd.read_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/bolsa_4acciones.csv')
 bYahoo = pd.read_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/Acciones_Yahoo.csv')
 
+#se importa función de Graficar.py para comparar ambas extracciones Bolsa Madrid y Yahoo finance
 grafcompar(bMadrid,bYahoo,'Variacion','Nombre',8,8)
 
 # Maximos y minimos de Bolsa de Madrid
@@ -18,8 +20,8 @@ AccionMaxGanancia = [bolsaMadrid['Nombre'][2],bolsaMadrid['Nombre'][3]]
 
 print(f'Las 2 mejores variaciones fueron las acciones: {AccionMaxGanancia[0]} y {AccionMaxGanancia[1]} con las variaciones {maxGanancia[0]} y {maxGanancia[1]}')
 print(f'Las 2 peores variaciones fueron las acciones: {AccionMinGanancia[0]} y {AccionMinGanancia[1]} con las variaciones {minGanancia[0]} y {minGanancia[1]}')
-# Maximos y minimos de Yahoo Finance
 
+# Maximos y minimos de Yahoo Finance
 bolsaYahoo = bYahoo.sort_values('Variacion')
 print(bolsaYahoo)
 minGanancia1 = [bolsaYahoo['Variacion'][0],bolsaYahoo['Variacion'][1]]
@@ -27,5 +29,6 @@ maxGanancia1 = [bolsaYahoo['Variacion'][2],bolsaYahoo['Variacion'][3]]
 AccionMinGanancia1 = [bolsaYahoo['Nombre'][0],bolsaYahoo['Nombre'][1]]
 AccionMaxGanancia1 = [bolsaYahoo['Nombre'][2],bolsaYahoo['Nombre'][3]]
 
+#Se imprimen dos mejores y peores variaciones de acciones de ambas bolsas por  cada accion
 print(f'Las 2 mejores variaciones fueron las acciones: {AccionMaxGanancia1[0]} y {AccionMaxGanancia1[1]} con las variaciones {maxGanancia1[0]} y {maxGanancia1[1]}')
 print(f'Las 2 peores variaciones fueron las acciones: {AccionMinGanancia1[0]} y {AccionMinGanancia1[1]} con las variaciones {minGanancia1[0]} y {minGanancia1[1]}')
--- TP2-WSyAPI/TP2-Parte_I/Yahoo_finance.py
+++ TP2-WSyAPI/TP2-Parte_I/Yahoo_finance.py
@@ -1,11 +1,11 @@
-
+# Se importan las librerias necesarias para extraer datos por API a Yahoo Finance
 import yfinance as yf
 import pandas as pd
 from matplotlib import pyplot as plt
 from yahoofinancials import YahooFinancials
 from Graficar import grafganper,traerdato
 
-
+# Mediante lista, se lista el nombre de acciones a extraer desde yfinance
 tickerStrings = ['SAN', 'TEF', 'REP.MC', 'BBVA']
 acciones = ['SANTANDER', 'TELEFONICA', 'REPSOL', 'BBVA']
 for ticker in tickerStrings:
@@ -13,24 +13,29 @@ for ticker in tickerStrings:
     data['ticker'] = ticker  # Agrego una columna con el nombre de los tickers
     data.to_csv(f'TP2-WSyAPI/TP2-Nuevoprueba/CSV/ticker_{ticker}.csv')  # creacion del CSV file
 
+# Se leen los csv's generados por cada acción para convertirlo en dataframes diferentes
 df1 = pd.read_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/ticker_SAN.csv')
 df2 = pd.read_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/ticker_TEF.csv')
 df3 = pd.read_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/ticker_REP.MC.csv')
 df4 = pd.read_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/ticker_BBVA.csv')
 
+# Se concatenan los df de cada acción y se guarda en otra variable
 data = pd.concat([df1, df2, df3, df4], ignore_index=True)
 data
 
+# Se define función para que retorne los datos en tipo float
 def varYahoo(ticker):
     data1 = YahooFinancials(ticker).get_current_change()
     return float(data1)
 
+# Se utilza función varYahoo para cada acción
 varYahoo = [varYahoo('SAN'),varYahoo('TEF'),varYahoo('REP.MC'),varYahoo('BBVA')]
 
 data.insert(1, "Nombre", acciones)
 data.insert(2, "Variacion", varYahoo)
 print(data)
 
+# Se convierte df data a csv
 data.to_csv('TP2-WSyAPI/TP2-Nuevoprueba/CSV/Acciones_Yahoo.csv')
 
 # Gráfico del valor de apertura de las acciones ultimos 5 dias.
@@ -39,6 +44,7 @@ tel_df = traerdato('TEF','5d')
 rep_df = traerdato('REP.MC','5d')
 bbva_df = traerdato('BBVA','5d')
 
+# Se configura gráfico de matplotlib para cada acción de los últimos 5 días
 plt.figure(figsize=(12,10))
 plt.subplot(2,2,1)
 bbva_df['Open'].plot(title="SANTANDER", marker="d", color='#01b8aa', label="Valor apertura acción");
